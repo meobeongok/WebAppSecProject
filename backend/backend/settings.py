23ba065eb5c2dbd5a29f0d8f47da1183fb4ca683
thihuynhdotexe <thaithi0211@gmail.com>
2022-05-17 00:12:37 +0700

    Backend: Add file type validators

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -130,10 +130,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-CORS_ALLOWED_ORIGINS = os.environ.get(
-    "DJANGO_CORS_ALLOWED_ORIGINS",
-    "http://localhost http://127.0.0.1 http://127.0.0.1:3000",
-).split(" ")
+CORS_ALLOWED_ORIGINS = os.environ.get("DJANGO_CORS_ALLOWED_ORIGINS", "http://localhost http://127.0.0.1").split(" ")
 
 CSRF_COOKIE_SECURE = True
 
